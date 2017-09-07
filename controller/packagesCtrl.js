4d12b7aa7763739147e09ff4110da739fcf2b964
Shiva Patel <shivapatel15@yahoo.com>
2017-09-07 19:50:50 -0400

    got everything working except for about and contact page and logo misalignment

---
+++ controller/packagesCtrl.js
@@ -0,0 +1,6 @@
+angular.module('devmtnTravel').controller('packagesCtrl', function($scope, $mainSrv) {
+
+
+$scope.packages = mainSrv.packageInfo;
+
+});
--- js/app.js
+++ js/app.js
@@ -1,11 +1,36 @@
-angular.module('devmtnTravel', ['ui.router'])
-    .config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('home',{
-                url:'/',
-                templateUrl: "../views/about.html"
-            });
+angular
+  .module("devmtnTravel", ["ui.router"])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state("home", {
+        url: "/",
+        templateUrl: "../views/about.html"
+      })
+      .state("adventurers", {
+        parent: 'home',
+        url: "/adventurers",
+        templateUrl: "../views/about-adventurers.html"
+      })
+      .state("contact", {
+        parent: 'home',
+        url: "/contact",
+        templateUrl: "../views/contact.html"
+      })
+      .state("packages", {
+        url: "/packages/:country",
+        templateUrl: "../views/packages.html",
+        controller: "packagesCtrl"
+      })
+      .state("locations", {
+        url: "/locations",
+        templateUrl: "../views/locations.html",
+        controller: "locationsCtrl"
+      })
+      .state("booked", {
+        url: "/booked/:id",
+        templateUrl: "../views/booked.html",
+        controller: "bookedCtrl"
+      });
 
-        $urlRouterProvider
-            .otherwise('/');
-    });
+   $urlRouterProvider.otherwise("/");
+  });
